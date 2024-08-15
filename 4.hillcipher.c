c8b3189a302aff6f497aabd0d7a7eff8402a3654
Pradyut30 <121106850+Pradyut30@users.noreply.github.com>
2024-08-15 14:09:20 +0530

    Update 4.hillcipher.c

--- 4.hillcipher.c
+++ 4.hillcipher.c
@@ -1,65 +1,118 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define MAX_LEN 100
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#define MAX_SIZE 100
 
-void encrypt(char *message, int key[][MAX_LEN], int n1, int n2) {
-    int i, j, k;
-    int len = strlen(message);
-    int cipher[len];
-    
-    // Convert each character to its numerical equivalent (A=0, B=1, ..., Z=25)
-    for (i = 0; i < len; i++) {
-        cipher[i] = message[i] - 'A';
+int is_valid_key(int key[MAX_SIZE][MAX_SIZE],int size)
+{
+    int det=0;
+    if(size==2)
+    {
+        det=key[0][0]*key[1][1]-key[0][1]*key[1][0];
     }
-    
-    // Pad the message with 'X' to make its length a multiple of n2
-    while (len % n2 != 0) {
-        message[len] = 'X';
-        cipher[len] = 23;  // 'X' is 23 in our number system
-        len++;
+    else if(size==3)
+    {
+        det=key[0][0]*(key[1][1]*key[2][2]-key[1][2]*key[2][1])-
+        key[0][1]*(key[1][0]*key[2][2]-key[1][2]*key[2][0])+
+        key[0][2]*(key[1][0]*key[2][1]-key[1][1]*key[2][0]);
     }
-    
-    
-    for (i = 0; i < len; i += n2) {
-        for (j = 0; j < n1; j++) {
-            int sum = 0;
-            for (k = 0; k < n2; k++) {
-                sum += key[j][k] * cipher[i + k];
-            }
-            encrypted_message[i + j] = (sum % 26) + 'A';
-        }
-    }
-    encrypted_message[len] = '\0';  // Null-terminate the encrypted message
- 
+    return det!=0;
 }
 
-int main() {
-    char message[MAX_LEN];
-    int n1, n2, i, j;
-
-    // Input key matrix dimensions
-    printf("Enter the number of rows in the key matrix: ");
-    scanf("%d", &n1);
-    printf("Enter the number of columns in the key matrix: ");
-    scanf("%d", &n2);
-
-    int key[MAX_LEN][MAX_LEN];
-    printf("Enter the key matrix:\n");
-    for (i = 0; i < n1; i++) {
-        for (j = 0; j < n2; j++) {
-            scanf("%d", &key[i][j]);
+void preprocess(char *message)
+{
+    int len=strlen(message);
+    int j=0;
+    for(int i=0;i<len;i++)
+    {
+        if(isalpha(message[i]))
+        {
+            message[j++]=toupper(message[i]);
         }
     }
+    message[j]='\0';
+}
 
-   
-    // Input the message to encrypt
-    printf("Enter the message you want to encrypt (uppercase letters only):\n");
-    fgets(message, sizeof(message), stdin);
-    message[strcspn(message, "\n")] = '\0';  // Remove the newline character
+void hillencrypt(char *message,int key[MAX_SIZE][MAX_SIZE],int size)
+{
+    int len=strlen(message);
+    int i,j,k,sum;
+    int plain[MAX_SIZE];
+    
+    while(len % size!=0)
+    {
+        message[len++]='X';
+    }
+    message[len]='\0';
+    
+    for(i=0;i<len;i+=size)
+    {
+        for(j=0;j<size;j++)
+        {
+            plain[j]=message[i+j]-'A';
+        }
+        
+        for(j=0;j<size;j++)
+        {
+            sum=0;
+            for(k=0;k<size;k++)
+            {
+                sum+=key[j][k]*plain[k];
+            }
+            message[i+j]=(sum % 26)+'A';
+        }
+        
+    }
+}
 
-    encrypt(message, key, n1, n2);
-
-    printf("The encrypted message is: %s\n", message);
+int main()
+{
+    int key[MAX_SIZE][MAX_SIZE];
+    char message[100];
+    int size,i,j,k;
+    
+    printf("enter the size of the matrix\n");
+    scanf("%d",&size);
+    
+    printf("enter the key matrix\n");
+    for(i=0;i<size;i++)
+    {
+        for(j=0;j<size;j++)
+        {
+            scanf("%d",&key[i][j]);
+        }
+    }
+    if(!is_valid_key(key,size))
+    {
+        printf("please enter a valid matrix\n");
+        return 1;
+    }
+    
+    printf("enter the message that you want to encrypt\n");
+    getchar();
+    fgets(message,sizeof(message),stdin);
+    message[strcspn(message,"\n")]='\0';
+    
+    preprocess(message);
+    hillencrypt(message,key,size);
+    printf("the encrypted message is %s\n",message);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
